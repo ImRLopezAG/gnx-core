0679dbe88f00ec0679eb809f2d1793e99ebeefa5
Angel Gabriel Lopez <angel178293lopez@gmail.com>
2023-12-23 09:22:45 -0400

    Refactor model templates

--- apps/cli/src/utils/templates/model.ts
+++ apps/cli/src/utils/templates/model.ts
@@ -5,34 +5,10 @@ interface ServiceTemplateOptions {
 
 export function SequelizeModelTemplate ({ name }: ServiceTemplateOptions): string {
   name = capitalizeFirstLetter(name)
-  return `import { SequelizeBaseEntity } from '@gnx-utilities/models'
-import { DataTypes } from 'sequelize'
-import { sequelize } from 'database'
-export class ${name} extends SequelizeBaseEntity {
-}
-
-${name}.init(
-  {
-    isDeleted: { type: DataTypes.BOOLEAN, defaultValue: false }
-  },
-  { sequelize, modelName: ${name} }
-)
-`
+  return `import { SequelizeBaseEntity } from '@gnx-utilities/models';\nimport { DataTypes } from 'sequelize';\nimport { sequelize } from 'database';\nexport class ${name} extends SequelizeBaseEntity {\n}\n\n${name}.init(\n  {\n    isDeleted: { type: DataTypes.BOOLEAN, defaultValue: false }\n  },\n  { sequelize, modelName: '${name}' }\n)\n`
 }
 
 export function TypegooseModelTemplate ({ name }: ServiceTemplateOptions): string {
   name = capitalizeFirstLetter(name)
-  return `import { getModelForClass, prop } from '@typegoose/typegoose'
-import { TypegooseBaseEntity } from '@gnx-utilities/models'
-
-export class ${name} extends TypegooseBaseEntity {
-  @prop({ type: String })
-  declare firstName: string
-
-  @prop({ type: String })
-  declare lastName: string
-}
-
-export const ${name}Model = getModelForClass(${name})
-`
+  return `import { getModelForClass, prop } from '@typegoose/typegoose';\nimport { TypegooseBaseEntity } from '@gnx-utilities/models';\n\nexport class ${name} extends TypegooseBaseEntity {\n  @prop({ type: String })\n  declare firstName: string;\n\n  @prop({ type: String })\n  declare lastName: string;\n}\n\nexport const ${name}Model = getModelForClass(${name});\n`
 }
